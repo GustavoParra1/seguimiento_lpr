04cafc2dee1ebf16f6f5d0c25c93ab422e8a1f14
GustavoParra1 <108834013+GustavoParra1@users.noreply.github.com>
2025-06-14 13:47:46 -0300

    Update seguimiento_lpr.py

--- seguimiento_lpr.py
+++ seguimiento_lpr.py
@@ -20,7 +20,7 @@ df_comunes = df[df['Tipo'].str.lower() == 'común']
 st.sidebar.title("Seguimiento desde LPR")
 camara_lpr_sel = st.sidebar.selectbox(
     "Seleccioná una cámara LPR",
-    df_lpr['id_cámara LPR'].unique()
+    df_lpr['id_camara LPR'].unique()
 )
 
 # Coordenadas base
